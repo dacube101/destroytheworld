be009d3e43a4c05f0d35fec43e009554417074da
dacube101 <rafael10123564@outlook.com>
2017-03-27 18:56:36 -0400

    hello world

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("help me!!!!!") 
     }
 
     override func didReceiveMemoryWarning() {
